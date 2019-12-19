d7bff95d90673ef683babef46b76d66aa5fd4b93
unknown <javiercursowebutrera@gmail.com>
2019-12-19 11:14:27 +0100

    Primera versión: Proyecto Autorización

---
+++ autorizacion.js
@@ -0,0 +1,31 @@
+function verificar(){
+	var usu = document.getElementById("usuario").value;
+
+	if(document.getElementById("usuario").value==""){
+		document.getElementById("mensaje").value="Debe escribir un nombre de usuario";
+	}
+	else if(usu == 'guess'){
+		document.getElementById("mensaje").value = "usuario autorizado";
+	}
+	else if(usu == 'invitado'){
+		document.getElementById("mensaje").value = "usuario autorizado";
+	}
+	else if(usu == 'usuario'){
+		document.getElementById("mensaje").value = "usuario autorizado";
+	}
+	else{
+		document.getElementById("mensaje").value = "usuario NO autorizado";
+	}
+}
+
+function salir(){
+	window.close();
+}
+
+
+
+
+
+
+
+
